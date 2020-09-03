37c3c5c5e39db7bae803421f146b8e40c356579d
Hamza <hamzam1478@gmail.com>
2020-09-03 14:53:11 +0100

    middlerow-updated
    I have updated the middle row so it looks a lot like twitter.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from "react";
 import "./App.css";
 import Feed from "./Feed";
+<<<<<<< Updated upstream
 
 function App() {
   return (
     <div className="app">
       <Feed />
     </div>
+=======
+import simpsons from "./simpsons.gif";
+import TweetBox from "./TweetBox.js";
+import imran from "./imran.png";
+import mouse from "./oldmickeyMouse.jpg";
+import history from "./hentri.jpg";
+import danielpfp from "./danielpfp.jpeg";
+import marriage from "./married.jpg";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="app">
+        <TweetBox />
+        <Feed
+          username={"@hamzam1478"}
+          sentence={"This is what coding does to me everyday."}
+          feedname={"Hamza Mahmood"}
+          image={simpsons}
+          avatar={mouse}
+        />
+        <div classname="imransfeed">
+          <Feed
+            username={"@imran123"}
+            sentence={
+              "Exploring through a historic landmark which was founded in 1918. This picture was taken in the resturant, which still operates today and looks really nice!"
+            }
+            feedname={"Imran Nawaz"}
+            image={history}
+            avatar={imran}
+          />
+          <Feed
+            username={"@Daniel145"}
+            sentence={
+              "My sister got married today, eyes full of tears of joy and tears. Mostly tears... Who am I going to annoy now :("
+            }
+            feedname={"Daniel Davis"}
+            image={marriage}
+            avatar={danielpfp}
+          />
+        </div>
+      </div>
+    </React.Fragment>
+>>>>>>> Stashed changes
   );
 }
 
--- src/Feed.js
+++ src/Feed.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Feed.css";
+<<<<<<< Updated upstream
 import TweetBox from "./TweetBox.js";
 import Post from "./Post.js";
 
 function Feed() {
+=======
+
+/* import Post from "./Post.js";
+ */
+function Feed(props) {
+>>>>>>> Stashed changes
   return (
     <div className="feed">
       {/* header */}
       <div className="feed__header">
+<<<<<<< Updated upstream
         <h2>HOME</h2>
 
         {/* tweetbox */}
@@ -18,6 +26,15 @@ function Feed() {
 
         {/* post */}
         {/* post */}
+=======
+        <h1>{props.name}</h1>
+        <img className="Avatar" src={props.avatar} alt="pfp"></img>
+        <h3>{props.username}</h3>
+        <h1>{props.feedname}</h1>
+        <h2>{props.sentence}</h2>
+        <img className="feedimg" src={props.image} alt="image"></img>
+        <h3>{props.username}</h3>
+>>>>>>> Stashed changes
       </div>
     </div>
   );
